d63cd3706b4482a4aae81c69b0310030a3b05c57
Shaunak12wankhade <shaunak12wankhade@ggmail.com>
2021-08-05 18:54:08 +0530

    added calculate empwage of total working days

--- empattendance.sh
+++ empattendance.sh
@@ -1,22 +1,40 @@
 #!/bin/bash -x
-isPartTime=1
-isFullTime=2
-totalSalary=0
-empRatePerHr=20
-WorkingDays=20
-for (( day=1; day<=$WorkingDays; day++ ))
+# constants
+IS_FULLTIME=1
+IS_PARTTIME=2
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=100
+
+#variables
+totalEmpHrs=0
+totalWorkingDays=0
+
+function getWorkingHours() {
+    case $1 in
+     $IS_FULLTIME)
+       empHrs=8
+     ;;
+     $IS_PARTTIME)
+       empHrs=4
+     ;;
+  esac
+       echo $empHrs
+}
+
+function calculateDailyWage() {
+        local workHrs=$1
+        wage=$(($EMP_RATE_PER_HR*$workHrs))
+        echo $wage
+}
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-empCheck=$((RANDOM%3))
-case $empCheck in
-        $isFullTime)
-        empHrs=8
-        ;;
-        $isPartTime)
-        empHrs=4
-        ;;
-        *)
-        empHrs=0
-        ;;
-esac
-salary=$(( $empHrs*$empRatePerHr ))
+            ((totalWorkingDays++))
+            empCheck=$((RANDOM%3))
+            workHours=$"(getWorkingHours $empCheck)"
+            totalEmpHrs=$($totalEmpHrs+$workHours)
+            empDailyWage[$totalWorkingDays]="$( calculateDailyWage $workHours )"
 done
+Wage=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+echo "Daily Wage :: " ${empDailyWage[@]}
