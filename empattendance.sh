d4462b57f3fcd999c8e39607e9a9fbcf5efc258a
Shaunak12wankhade <shaunak12wankhade@ggmail.com>
2021-08-05 15:46:02 +0530

    added calculatewage

--- empattendance.sh
+++ empattendance.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+empRatePerHr=20
+empHrs=8
 empcheck=$((RANDOM%2))
 if [ $empcheck -eq 1 ]
 then
+wage=$(($empRatePerHr*$empHr))
 echo "emp is present"
 else
+wage=0
 echo "emp is absent"
 fi
