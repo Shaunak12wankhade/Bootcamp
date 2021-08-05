426e0ad4ae64faba082637f9094feb8f4ba59b4a
Shaunak12wankhade <shaunak12wankhade@ggmail.com>
2021-08-05 17:04:49 +0530

    added calculatewageforparttime

--- empattendance.sh
+++ empattendance.sh
@@ -1,12 +1,22 @@
-#!/bin/bash
+#!/bin/bash -x
+isPartTime=1
+isFullTime=2
+totalSalary=0
 empRatePerHr=20
-empHrs=8
-empcheck=$((RANDOM%2))
-if [ $empcheck -eq 1 ]
-then
-wage=$(($empRatePerHr*$empHr))
-echo "emp is present"
-else
-wage=0
-echo "emp is absent"
-fi
+WorkingDays=20
+for (( day=1; day<=$WorkingDays; day++ ))
+do
+empCheck=$((RANDOM%3))
+case $empCheck in
+        $isFullTime)
+        empHrs=16
+        ;;
+        $isPartTime)
+        empHrs=8
+        ;;
+        *)
+        empHrs=0
+        ;;
+esac
+salary=$(( $empHrs*$empRatePerHr ))
+done
